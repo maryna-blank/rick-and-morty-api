8d273b11d711b6dbcebf050648a5155ee4d33788
Maryna Blank <marinablank96@gmail.com>
2022-09-19 13:18:38 +0300

    base

---
+++ src/main/java/springboot/practice/rickandmortyapp/RickAndMortyAppApplication.java
@@ -0,0 +1,13 @@
+package springboot.practice.rickandmortyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/practice/rickandmortyapp/RickAndMortyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.practice.rickandmortyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
